946e88f37c5a201ef71e8b80a0c2ab7dc6ce82f0
unknown <rajatis1999@gmail.com>
2020-07-05 02:37:21 +0530

    first commit. Not connected to main database but chat is working all fine

--- server.js
+++ server.js
@@ -4,46 +4,112 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
+var moment = require('moment');
+const { send } = require('process');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
 var Message = mongoose.model('Message',{
-  name : String,
-  message : String
+  id:String,
+  messages:[
+    {
+      mid:String,
+      name:String,
+      message:String,
+      date:String,
+      time:String
+    }
+  ]
 })
 
-var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
+// var dbUrl = 'mongodb+srv://admin:admin@cluster0-nbxxl.mongodb.net/simplechat?retryWrites=true&w=majority'
+mongoose.connect("mongodb+srv://admin:admin@cluster0-nbxxl.mongodb.net/chatms?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology:true});
 
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
+app.get('/messages/:userid1/:userid2', (req, res) => {
+
+  const messageId = req.params.userid1 + ',' + req.params.userid2;
+
+  Message.findOne({id:messageId},(err, messages)=> {
+    if(messages)
+    res.send(messages.messages);
+    else
+    res.send(null);
   })
 })
 
 
-app.get('/messages/:user', (req, res) => {
-  var user = req.params.user
-  Message.find({name: user},(err, messages)=> {
-    res.send(messages);
-  })
-})
+// app.get('/messages/:user', (req, res) => {
+//   var user = req.params.user
+//   Message.find({name: user},(err, messages)=> {
+//     res.send(messages);
+//   })
+// })
 
 
-app.post('/messages', async (req, res) => {
+app.post('/messages/:userid1/:userid2', async (req, res) => {
+  const messageId = req.params.userid1 + ',' + req.params.userid2;
+  var sendingGuy= messageId.substr(0, messageId.indexOf(','));
+  var sendingGuy2= messageId.substr(messageId.indexOf(',') +1, messageId.length);
+  console.log(sendingGuy2);
+  
+  // console.log('sending Guy = ' + sendingGuy + ' or ' + sendingGuy2);
+  var name = req.body.name;
+  var msg = req.body.message;
+
+  if((sendingGuy!=name)&&(sendingGuy2!=name))
+  {
+    console.log('Invalid request');
+    
+    return res.send('Invalid Request.');
+  }
+    
+
   try{
-    var message = new Message(req.body);
+    var uniqueNumber=(new Date().getTime()).toString(36);
 
-    var savedMessage = await message.save()
+    var message = {
+      mid: uniqueNumber,
+      name:name,
+      message:msg,
+      time:moment().format('h:mm a'),
+      date:moment(new Date()).format("DD/MM/YYYY")
+    };
+
+    Message.findOne({id:messageId}, async(err, found) => {
+      if(err){
+        res.sendStatus(500);
+        return console.log('error',err);
+      }
+      if(!found)
+      {
+        const newMessageRoom = new Message({
+          id:messageId
+        });
+        await newMessageRoom.save();
+        await newMessageRoom.messages.push(message);
+        await newMessageRoom.save();
+        console.log('saved');
+      }
+      else{
+      await found.messages.push(message);
+      await found.save();
       console.log('saved');
-
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
+      }
+      io.emit('message', message);
       res.sendStatus(200);
+    });
+
+    // var savedMessage = await message.save()
+      // console.log('saved');
+
+    // var censored = await Message.findOne({message:'badword'});
+    //   if(censored)
+    //     await Message.remove({_id: censored.id})
+    //   else
+    //     io.emit('message', req.body);
+    //   res.sendStatus(200);
   }
   catch (error){
     res.sendStatus(500);
@@ -52,7 +118,6 @@ app.post('/messages', async (req, res) => {
   finally{
     console.log('Message Posted')
   }
-
 })
 
 
@@ -61,9 +126,9 @@ io.on('connection', () =>{
   console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
-  console.log('mongodb connected',err);
-})
+// mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+//   console.log('mongodb connected',err);
+// })
 
 var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
